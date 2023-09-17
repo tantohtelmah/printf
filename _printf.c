5b8cac232f77fad626e5ff3dcaef59399f5e2df9
tantohtelmah <tantohtelmah@gmail.com>
2023-09-17 11:10:06 -0400

    Wriiten base code and tested with main.c, added putchar and header file

---
+++ _printf.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+* _printf - prints output to standard output according to a format
+* @format: character
+* Return: integer
+*/
+
+int _printf(const char *format, ...)
+{
+	/* Declaring variables */
+	va_list args;
+
+	va_start(args, format);
+	vfprint(stdout, format, args);
+	va_end(args);
+
+}
---
+++ main.c
@@ -0,0 +1,20 @@
+/*#include <limits.h>*/
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    /*int len;
+    int len2;
+    unsigned int ui;
+    void *addr;*/
+
+    printf("Let's try to printf a simple sentence.\n");
+    printf("Let's try to printf a simple sentence.\n");
+	return (0);
+}
