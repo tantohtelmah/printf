1a182ac0d064fd21fced943c4591f15d81fccac9
technoryta <technoryta@gmail.com>
2023-09-17 18:41:06 -0700

    added buffer to putchar

--- _putchar.c
+++ _putchar.c
@@ -9,5 +9,30 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+	return (buffering(c));
 }
+
+/**
+ * buffering - saves characters in a buffer before printing
+ * @c: character to print
+ *
+ * Return: 1 on success
+ */
+int buffering(char c)
+{
+	const int BUFF_SIZE = 1024;
+	static char buffer[BUFF_SIZE];
+	static int index;
+
+	if (index == BUFF_SIZE || c == -1)
+	{
+		write(1, buffer, index)
+		index = 0;
+	}
+
+	if (c != -1)
+		buffer[index++] = c;
+
+	return (1);
+}
+
